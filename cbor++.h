9f0f4d43c00208ddee1bfe55a11007264e0b6a4e
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2021-10-30 16:04:04 +1100

    some initial concepts

---
+++ cbor++.h
@@ -0,0 +1,817 @@
+/*
+ * cbor++ - CBOR C++ library
+ *
+ * Copyright 2021 Patrick Oppenlander <patrick.oppenlander@gmail.com>
+ *
+ * SPDX-License-Identifier: 0BSD
+ *
+ * See RFC8949.
+ */
+#pragma once
+
+#include <array>
+#include <bit>
+#include <cassert>
+#include <cmath>
+#include <iterator>
+#include <span>
+
+namespace cbor {
+
+/*
+ * unreachable - tell the compiler about unreachable code paths
+ */
+#define unreachable __builtin_unreachable
+
+/*
+ * literals
+ */
+inline namespace literals {
+
+constexpr
+std::byte
+operator""_b(unsigned long long v)
+{
+	return static_cast<std::byte>(v);
+}
+
+}
+
+/*
+ * type - C++(ish) mapping of CBOR data type
+ */
+enum class type {
+	int32,
+	int64,
+	bytes,
+	string,
+	array,
+	map,
+	tag,
+	uint32,
+	uint64,
+	bignum,
+	fp32,
+	fp64,
+	boolean,
+	null,
+	undefined,
+	indefinite_break,
+};
+
+/*
+ * ih - definitions & functions related to the CBOR item head
+ *
+ * A CBOR item head is made up of an inital byte containing a 3-bit major type
+ * and 5 bits of additional information. This is followed by 0, 1, 2, 4 or 8
+ * additional bytes.
+ */
+namespace ih {
+
+/*
+ * major - item head major type
+ */
+enum class major {
+	posint = 0,
+	negint = 1,
+	bytes = 2,
+	utf8 = 3,
+	array = 4,
+	map = 5,
+	tag = 6,
+	special = 7,
+};
+
+/* Aligning these improves code generation */
+static_assert(static_cast<int>(major::bytes) == static_cast<int>(type::bytes));
+static_assert(static_cast<int>(major::utf8) == static_cast<int>(type::string));
+static_assert(static_cast<int>(major::array) == static_cast<int>(type::array));
+static_assert(static_cast<int>(major::map) == static_cast<int>(type::map));
+static_assert(static_cast<int>(major::tag) == static_cast<int>(type::tag));
+
+/*
+ * ai - item head additional information values
+ */
+enum class ai {
+	/* 0 - 23 directly encoded value */
+	byte = 24,
+	word = 25,
+	dword = 26,
+	qword = 27,
+	/* 28 - 30 reserved */
+	indefinite = 31,    /* only valid for bytes, utf8 and special */
+};
+
+/*
+ * special - CBOR special data item type
+ *
+ * Stored in additional information of data item head.
+ */
+enum class special {
+	bool_false = 20,
+	bool_true = 21,
+	null = 22,
+	undefined = 23,
+	extended = 24,
+	fp16 = 25,
+	fp32 = 26,
+	fp64 = 27,
+	indefinite_break = 31,
+};
+
+/*
+ * make - create an item head
+ */
+constexpr
+std::byte
+make(major m, auto a)
+{
+	/* when this function is always evaluated in a constexpr context
+	 * this won't throw -  it will fail to compile */
+	if (static_cast<int>(m) > 7)
+		throw std::invalid_argument("invalid major");
+	if (static_cast<int>(a) > 31)
+		throw std::invalid_argument("invalid additional information");
+	return static_cast<std::byte>(m) << 5 |
+	    static_cast<std::byte>(a);
+}
+
+/*
+ * some useful item head constants
+ */
+constexpr auto fp16 = make(major::special, special::fp16);
+constexpr auto fp32 = make(major::special, special::fp32);
+constexpr auto fp64 = make(major::special, special::fp64);
+constexpr auto null = make(major::special, special::null);
+constexpr auto bool_false = make(major::special, special::bool_false);
+constexpr auto bool_true = make(major::special, special::bool_true);
+
+/*
+ * get_major - get major type from CBOR item head at 'p'
+ */
+inline
+major
+get_major(auto p)
+{
+	static_assert(sizeof *p == 1);
+
+	return static_cast<major>(static_cast<int>(*p) >> 5);
+}
+
+/*
+ * get_ai - get additional information from CBOR item head at 'p'
+ */
+inline
+unsigned
+get_ai(auto p)
+{
+	static_assert(sizeof *p == 1);
+
+	return static_cast<unsigned>(*p) & 0x1f;
+}
+
+/*
+ * get_arg_size - get size of argument from CBOR item head at 'p'
+ */
+inline
+unsigned
+get_arg_size(auto p)
+{
+	static_assert(sizeof *p == 1);
+
+	switch (get_ai(p)) {
+	case 0 ... 23:
+		return 0;
+	case static_cast<int>(ai::byte):
+		return 1;
+	case 25:
+		return 2;
+	case 26:
+		return 4;
+	case 27:
+		return 8;
+	case 28 ... 30:
+		throw std::runtime_error("invalid additional information");
+	case 31:
+		return 0;
+	default:
+		unreachable();
+	}
+}
+
+/*
+ * get_ih_size - get size (in bytes) of CBOR item head at 'p'
+ */
+inline
+unsigned
+get_ih_size(auto p)
+{
+	static_assert(sizeof *p == 1);
+
+	return 1 + get_arg_size(p);
+}
+
+/*
+ * get_arg - get argument of CBOR item head at 'p'
+ */
+inline
+uint64_t
+get_arg(auto p)
+{
+	auto ai = get_ai(p);
+	switch (ai) {
+	case 0 ... 23:
+		return ai;
+	case 24:
+		return static_cast<uint8_t>(p[1]);
+	case 25:
+		return read_be<uint16_t>(p + 1);
+	case 26:
+		return read_be<uint32_t>(p + 1);
+	case 27:
+		return read_be<uint64_t>(p + 1);
+	case 28 ... 30:
+		throw std::runtime_error("invalid short count");
+	case 31:
+		return 31;
+	default:
+		unreachable();
+	}
+}
+
+/*
+ * get_data_size - get data size of CBOR item at 'p'
+ */
+inline
+uint64_t
+get_data_size(auto p)
+{
+	switch (get_major(p)) {
+	case major::bytes:
+	case major::utf8:
+		return get_arg(p);
+	default:
+		return 0;
+	}
+}
+
+/*
+ * get_size - get size of CBOR item at 'p'
+ */
+inline
+uint64_t
+get_size(auto p)
+{
+	return get_ih_size(p) + get_data_size(p);
+}
+
+}
+
+/*
+ * byteswap - byteswap a value
+ *
+ * Unfortunately current versions of gcc (11.2) and clang (13.0.0) generally
+ * don't optimise std::reverse down to byteswap instructions, and std::byteswap
+ * isn't available until c++23.
+ *
+ * There are countless ways to write this function. The following tries to use
+ * as much standard c++ as possible and avoid casting and undefined behaviour.
+ */
+inline
+auto
+byteswap(auto v)
+{
+	auto vb = as_writable_bytes(std::span(&v, 1));
+	std::span<std::byte> tb;
+	union {
+		uint16_t t16;
+		uint32_t t32;
+		uint64_t t64;
+	};
+
+	static_assert(sizeof v == 2 || sizeof v == 4 || sizeof v == 8,
+		      "Are you sure you want to byteswap that?");
+
+	switch (sizeof v) {
+	case 2:
+		tb = as_writable_bytes(std::span(&t16, 1));
+		std::copy(begin(vb), end(vb), begin(tb));
+		t16 = __builtin_bswap16(t16);
+		break;
+	case 4:
+		tb = as_writable_bytes(std::span(&t32, 1));
+		std::copy(begin(vb), end(vb), begin(tb));
+		t32 = __builtin_bswap32(t32);
+		break;
+	case 8:
+		tb = as_writable_bytes(std::span(&t64, 1));
+		std::copy(begin(vb), end(vb), begin(tb));
+		t64 = __builtin_bswap64(t64);
+		break;
+	}
+
+	std::copy(begin(tb), end(tb), begin(vb));
+	return v;
+}
+
+/*
+ * write_be - write a big endian value to byte iterator 'p'
+ */
+inline
+void
+write_be(auto p, auto v)
+{
+	auto vb = as_writable_bytes(std::span(&v, 1));
+	if (std::endian::native != std::endian::big) {
+		/* equivalent to std::reverse(begin(vb), end(vb)) but
+		 * optimises properly */
+		v = byteswap(v);
+	}
+	std::copy(begin(vb), end(vb), p);
+}
+
+/*
+ * read_be - read a big endian value from byte iterator 'p'
+ */
+template<class T>
+T
+read_be(auto p)
+{
+	T v;
+	auto vb = as_writable_bytes(std::span(&v, 1));
+	std::copy(p, p + sizeof v, begin(vb));
+	if (std::endian::native != std::endian::big) {
+		/* equivalent to std::reverse(begin(vb), end(vb)) but
+		 * optimises properly */
+		v = byteswap(v);
+	}
+	return v;
+}
+
+/*
+ * data_item
+ */
+template<class I>
+class data_item {
+public:
+	data_item(I p)
+	: p_{p}
+	{ }
+
+	template<class T>
+	requires std::is_integral_v<T>
+	T
+	get() const
+	{
+		/* booleans */
+		if (std::is_same_v<T, bool>) {
+			switch (*p_) {
+			case ih::bool_false:
+				return false;
+			case ih::bool_true:
+				return true;
+			default:
+				throw std::runtime_error("data is not boolean");
+			}
+		}
+
+		/* integers */
+		T v;
+		switch (ih::get_major(p_)) {
+		case ih::major::posint:
+			v = ih::get_arg(p_);
+			if (static_cast<uint64_t>(v) != ih::get_arg(p_))
+				throw std::range_error("integer overflow");
+			return v;
+		case ih::major::negint:
+			if (ih::get_arg(p_) > std::numeric_limits<int64_t>::max())
+				throw std::range_error("integer overflow");
+			v = -1 - static_cast<int64_t>(ih::get_arg(p_));
+			if (v != -1 - static_cast<int64_t>(ih::get_arg(p_)))
+				throw std::range_error("integer overflow");
+			return v;
+		default:
+			throw std::runtime_error("data is not integral");
+		}
+	}
+
+	template<class T>
+	requires std::is_floating_point_v<T>
+	T
+	get() const
+	{
+		T v;
+		switch (*p_) {
+		case ih::fp16:
+			if (p_[2] != 0x00_b)
+				throw std::runtime_error("float16 not supported");
+			if (p_[1] == 0x7e_b)
+				return std::numeric_limits<T>::quiet_NaN();
+			if (p_[1] == 0x7c_b)
+				return std::numeric_limits<T>::infinity();
+			if (p_[1] == 0xfc_b)
+				return -std::numeric_limits<T>::infinity();
+			throw std::runtime_error("float16 not supported");
+		case ih::fp32:
+			return read_be<float>(p_);
+		case ih::fp64:
+			v = read_be<double>(p_);
+			if (v != read_be<double>(p_))
+				throw std::range_error("lossy float conversion");
+			return v;
+		default:
+			throw std::runtime_error("data is not floating point");
+		}
+	}
+
+	cbor::type
+	type() const
+	{
+		switch (ih::get_major(p_)) {
+		case ih::major::posint: {
+			auto sz = ih::get_arg_size(p_);
+			auto val = ih::get_arg(p_);
+			if (sz < 4)
+				return cbor::type::int32;
+			if (sz == 4)
+				return val > std::numeric_limits<int32_t>::max()
+				    ? cbor::type::uint32 : cbor::type::int32;
+			return val > std::numeric_limits<int64_t>::max()
+			    ? cbor::type::uint64 : cbor::type::int64;
+		}
+		case ih::major::negint: {
+			auto sz = ih::get_arg_size(p_);
+			auto val = ih::get_arg(p_);
+			if (sz < 4)
+				return cbor::type::int32;
+			return val > std::numeric_limits<int32_t>::max() ?
+			    cbor::type::int64 : cbor::type::int32;
+		}
+		case ih::major::bytes:
+			return cbor::type::bytes;
+		case ih::major::utf8:
+			return cbor::type::string;
+		case ih::major::array:
+			return cbor::type::array;
+		case ih::major::map:
+			return cbor::type::map;
+		case ih::major::tag:
+			return cbor::type::tag;
+		case ih::major::special:
+			switch (static_cast<ih::special>(ih::get_ai(p_))) {
+			case ih::special::bool_false:
+			case ih::special::bool_true:
+				return cbor::type::boolean;
+			case ih::special::null:
+				return cbor::type::null;
+			case ih::special::undefined:
+				return cbor::type::undefined;
+			case ih::special::extended:
+				throw std::runtime_error("extended special types are not supported");
+			case ih::special::fp16:
+			case ih::special::fp32:
+				return cbor::type::fp32;
+			case ih::special::fp64:
+				return cbor::type::fp64;
+			case ih::special::indefinite_break:
+				return cbor::type::indefinite_break;
+			default:
+				throw std::runtime_error("invalid special type");
+			}
+		default:
+			unreachable();
+		}
+	}
+
+private:
+	I p_;
+};
+
+/*
+ * codec - raw CBOR codec
+ *
+ * Indexes refer to CBOR data items.
+ * Iterators iterate CBOR data items.
+ *
+ * This is for low level manipulation of CBOR encoded data.
+ */
+template<class S>
+class codec {
+public:
+	using reference = data_item<typename S::iterator>;
+	using const_reference = const data_item<typename S::const_iterator>;
+	using difference_type = std::ptrdiff_t;
+	using size_type = std::size_t;
+
+	/*
+	 * iterators
+	 */
+	template<bool const__>
+	class itr__ {
+		friend class codec;
+
+	public:
+		using container = std::conditional_t<const__, const codec, codec>;
+		using difference_type = codec<S>::difference_type;
+		using reference = std::conditional_t<const__,
+						     codec<S>::const_reference,
+						     codec<S>::reference>;
+		using iterator_category = std::random_access_iterator_tag;
+
+		itr__()
+		: it_{}
+		, c_{}
+		{ }
+
+		itr__(size_type it, container *c)
+		: it_{it}
+		, c_{c}
+		{ }
+
+		itr__(const itr__ &) = default;
+		~itr__() = default;
+		itr__ &operator=(const itr__ &) = default;
+
+		/* iterator->const_iterator conversion */
+		template<bool c = const__, class = std::enable_if_t<c>>
+		itr__(const itr__<false> &r)
+		: it_{r.it_}
+		, c_{r.c_}
+		{ }
+
+		std::strong_ordering
+		operator<=>(const itr__ &r) const
+		{
+			return it_ <=> r.it_;
+		}
+
+		bool
+		operator==(const itr__ &r) const
+		{
+			return it_ == r.it_;
+		}
+
+		itr__ &
+		operator++()
+		{
+			++it_;
+			return *this;
+		}
+
+		itr__
+		operator++(int)
+		{
+			auto tmp{*this};
+			++it_;
+			return tmp;
+		}
+
+		itr__ &
+		operator--()
+		{
+			--it_;
+			return *this;
+		}
+
+		itr__
+		operator--(int)
+		{
+			auto tmp{*this};
+			--it_;
+			return tmp;
+		}
+
+		itr__ &
+		operator+=(size_type d)
+		{
+			it_ += d;
+			return *this;
+		}
+
+		itr__
+		operator+(size_type d) const
+		{
+			auto tmp{*this};
+			return tmp += d;
+		}
+
+		friend
+		itr__
+		operator+(size_type d, const itr__ &i)
+		{
+			return i + d;
+		}
+
+		itr__ &
+		operator-=(size_type d)
+		{
+			it_ -= d;
+			return *this;
+		}
+
+		itr__
+		operator-(size_type d) const
+		{
+			auto tmp{*this};
+			return tmp -= d;
+		}
+
+		difference_type
+		operator-(const itr__<true> &r) const
+		{
+			return it_ - r.it_;
+		}
+
+		reference
+		operator*() const
+		{
+			return c_->ref(it_);
+		}
+
+		reference
+		operator[](size_type d) const
+		{
+			return c_->ref(it_ + d);
+		}
+
+	private:
+		size_type it_;
+		container *c_;
+	};
+	typedef itr__<false> iterator;
+	typedef itr__<true> const_iterator;
+	typedef std::reverse_iterator<iterator> reverse_iterator;
+	typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
+	/*
+	 * Constructor for CBOR codec using storage container S.
+	 */
+	codec(S &s)
+	: s_{s}
+	{ }
+
+	/* codec &operator=(const codec&); */
+	/* bool operator==(const codec&) const; */
+	/* bool operator!=(const codec&) const; */
+	/* iterator begin() */
+	/* const_iterator begin() const */
+	/* const_iterator cbegin() const */
+	/* iterator end() */
+	/* const_iterator end() const */
+	/* const_iterator cend() const */
+	/* reverse_iterator rbegin() */
+	/* const_reverse_iterator rbegin() const */
+	/* const_reverse_iterator crbegin() const */
+	/* reverse_iterator rend() */
+	/* const_reverse_iterator rend() const */
+	/* const_reverse_iterator crend() const */
+	/* reference front() */
+	/* const_reference front() const */
+	/* reference back() */
+	/* const_reference back() const */
+	/* template<typename ...A> void emplace_front(A &&...a) */
+	/* template<typename ...A> void emplace_back(A &&...a) */
+	/* void push_front(const T &v) */
+	/* void push_front(T &&v) */
+
+	void
+	push_back(const auto &v)
+	{
+		encode(end(s_), v);
+	}
+
+	/* void push_back(const T &v) */
+	/* void push_back(T &&v) */
+	/* void pop_front() */
+	/* void pop_back() */
+
+	reference
+	operator[](size_type i)
+	{
+#warning fixme: stupid version just to get tests running
+		auto p = begin(s_);
+		auto len = ih::get_size(p);
+		for (size_type n = 0; n < i; ++n)
+			len = ih::get_size(p += len);
+		return {p};
+	}
+
+	/* const_reference operator[](size_type i) const */
+	/* reference at(size_type); */
+	/* const_reference at(size_type) const; */
+	/* template<class ...A> iterator emplace(const_iterator pos, A &&...a) */
+	/* iterator insert(const_iterator pos, const T &v) */
+	/* iterator insert(const_iterator pos, T &&v) */
+	/* iterator insert(const_iterator pos, size_type n, const T &v) */
+	/* template<class I> iterator insert(const_iterator pos, I b, I e) */
+	/* iterator insert(const_iterator pos, std::initializer_list<T> l) */
+	/* iterator erase(const_iterator pos) */
+	/* iterator erase(const_iterator begin, const_iterator end) */
+
+	void
+	clear()
+	{
+		return s_.clear();
+	}
+
+	/* template<class it> void assign(iter, iter); */
+	/* void assign(std::initializer_list<T>); */
+	/* void assign(size_type, const T&); */
+	/* void swap(codec &); */
+
+	size_type
+	size() const
+	{
+#warning fixme: stupid version just to get tests running
+		size_type n = 0;
+		if (s_.empty())
+			return 0;
+		for (auto p = begin(s_); p != end(s_); p += ih::get_size(p))
+			++n;
+		return n;
+	}
+
+	/* size_type max_size() const */
+
+	bool empty() const
+	{
+		return s_.empty();
+	}
+
+private:
+	template<class T>
+	requires std::is_floating_point_v<T>
+	void
+	encode(S::iterator p, T v)
+	{
+		/* nan is encoded as 2-byte float */
+		if (std::isnan(v)) {
+			auto b = {ih::fp16, 0x7e_b, 0x00_b};
+			s_.insert(p, begin(b), end(b));
+			return;
+		}
+
+		/* infinity is encoded as 2-byte float */
+		if (std::isinf(v)) {
+			auto b = {ih::fp16, v > 0 ? 0x7c_b : 0xfc_b, 0x00_b};
+			s_.insert(p, begin(b), end(b));
+			return;
+		}
+
+		/* encode doubles as float when no data is lost */
+		if (std::is_same_v<T, double> && v == static_cast<float>(v)) {
+			encode(p, static_cast<float>(v));
+			return;
+		}
+
+		std::array<std::byte, sizeof v + 1> b{
+				  sizeof v == 4 ? ih::fp32 : ih::fp32};
+		write_be(&b[1], &v);
+		s_.insert(p, begin(b), end(b));
+	}
+
+	template<class T>
+	requires std::is_integral_v<T>
+	void
+	encode(S::iterator p, T v)
+	{
+		auto mt = v < 0 ? ih::major::negint : ih::major::posint;
+		std::make_unsigned_t<T> u = v < 0 ? -v - 1 : v;
+		if (u <= 23)
+			s_.insert(p, ih::make(mt, u));
+		else if (u <= std::numeric_limits<uint8_t>::max()) {
+			std::array<std::byte, 2> b{
+				ih::make(mt, ih::ai::byte),
+				static_cast<std::byte>(u)};
+			s_.insert(p, begin(b), end(b));
+		} else if (u <= std::numeric_limits<uint16_t>::max()) {
+			std::array<std::byte, 3> b{ih::make(mt, ih::ai::word)};
+			write_be(&b[1], static_cast<uint16_t>(u));
+			s_.insert(p, begin(b), end(b));
+		} else if (u <= std::numeric_limits<uint32_t>::max()) {
+			std::array<std::byte, 5> b{ih::make(mt, ih::ai::dword)};
+			write_be(&b[1], static_cast<uint32_t>(u));
+			s_.insert(p, begin(b), end(b));
+		} else {
+			std::array<std::byte, 9> b{ih::make(mt, ih::ai::qword)};
+			write_be(&b[1], u);
+			s_.insert(p, begin(b), end(b));
+		}
+	}
+
+	void
+	encode(S::iterator p, nullptr_t)
+	{
+		s_.insert(p, ih::null);
+	}
+
+	void
+	encode(S::iterator p, bool v)
+	{
+		s_.insert(p, v ? ih::bool_true : ih::bool_false);
+	}
+
+	S &s_;
+};
+
+}
---
+++ test/test.cpp
@@ -0,0 +1,372 @@
+#include "../cbor++.h"
+
+#include <gtest/gtest.h>
+
+using namespace cbor::literals;
+
+/*
+ * https://stackoverflow.com/questions/39049803/google-test-cant-find-user-provided-equality-operator
+ *
+ * explains the reasons for this nasty hack.
+ */
+namespace testing::internal {
+
+template<class T, std::size_t N>
+bool
+operator==(const std::vector<T> &l, const std::array<T, N> &r)
+{
+	if (size(l) != size(r))
+		return false;
+	return std::equal(begin(l), end(l), begin(r));
+}
+
+}
+
+TEST(cbor, codec_null)
+{
+	std::vector<std::byte> buf;
+	cbor::codec c(buf);
+
+	/* encode */
+	c.push_back(nullptr);
+
+	/* verify */
+	std::array exp{
+		0xf6_b		// null
+	};
+	ASSERT_EQ(buf, exp);
+
+	/* type checks */
+	EXPECT_EQ(c[0].type(), cbor::type::null);
+	EXPECT_THROW(c[0].get<bool>(), std::runtime_error);
+	EXPECT_THROW(c[0].get<int>(), std::runtime_error);
+	EXPECT_THROW(c[0].get<float>(), std::runtime_error);
+}
+
+TEST(cbor, codec_bool_true)
+{
+	std::vector<std::byte> buf;
+	cbor::codec c(buf);
+
+	/* encode */
+	c.push_back(true);
+
+	/* verify */
+	std::array exp{
+		0xf5_b		// true
+	};
+	ASSERT_EQ(buf, exp);
+
+	/* decode */
+	EXPECT_TRUE(c[0].get<bool>());
+
+	/* type checks */
+	EXPECT_EQ(c[0].type(), cbor::type::boolean);
+	EXPECT_THROW(c[0].get<int>(), std::runtime_error);
+	EXPECT_THROW(c[0].get<float>(), std::runtime_error);
+
+}
+
+TEST(cbor, codec_bool_false)
+{
+	std::vector<std::byte> buf;
+	cbor::codec c(buf);
+
+	/* encode */
+	c.push_back(false);
+
+	/* verify */
+	std::array exp{
+		0xf4_b		// false
+	};
+	ASSERT_EQ(buf, exp);
+
+	/* decode */
+	EXPECT_FALSE(c[0].get<bool>());
+
+	/* type checks */
+	EXPECT_EQ(c[0].type(), cbor::type::boolean);
+	EXPECT_THROW(c[0].get<int>(), std::runtime_error);
+	EXPECT_THROW(c[0].get<float>(), std::runtime_error);
+}
+
+TEST(cbor, codec_fp_nan)
+{
+	std::vector<std::byte> buf;
+	cbor::codec c(buf);
+
+	/* encode */
+	c.push_back(std::numeric_limits<float>::quiet_NaN());
+	c.push_back(std::numeric_limits<double>::quiet_NaN());
+
+	/* verify */
+	std::array exp{
+		0xf9_b, 0x7e_b, 0x00_b,	    // fp16 NaN
+		0xf9_b, 0x7e_b, 0x00_b	    // fp16 NaN
+	};
+	ASSERT_EQ(buf, exp);
+
+	/* decode */
+	EXPECT_TRUE(std::isnan(c[0].get<float>()));
+	EXPECT_TRUE(std::isnan(c[1].get<double>()));
+
+	/* type checks */
+	for (size_t i = 0; i < size(c); ++i) {
+		EXPECT_EQ(c[i].type(), cbor::type::fp32);
+		EXPECT_THROW(c[i].get<bool>(), std::runtime_error);
+		EXPECT_THROW(c[i].get<int>(), std::runtime_error);
+	}
+}
+
+TEST(cbor, codec_fp_positive_infinity)
+{
+	std::vector<std::byte> buf;
+	cbor::codec c(buf);
+
+	/* encode */
+	c.push_back(std::numeric_limits<float>::infinity());
+	c.push_back(std::numeric_limits<double>::infinity());
+
+	/* verify */
+	std::array exp{
+		0xf9_b, 0x7c_b, 0x00_b,	    // fp16 +infinity
+		0xf9_b, 0x7c_b, 0x00_b	    // fp16 +infinity
+	};
+	ASSERT_EQ(buf, exp);
+
+	/* decode */
+	EXPECT_GT(c[0].get<float>(), 0);
+	EXPECT_TRUE(std::isinf(c[0].get<float>()));
+	EXPECT_GT(c[1].get<double>(), 0);
+	EXPECT_TRUE(std::isinf(c[0].get<double>()));
+
+	/* type checks */
+	for (size_t i = 0; i < size(c); ++i) {
+		EXPECT_EQ(c[i].type(), cbor::type::fp32);
+		EXPECT_THROW(c[i].get<bool>(), std::runtime_error);
+		EXPECT_THROW(c[i].get<int>(), std::runtime_error);
+	}
+}
+
+TEST(cbor, codec_negative_infinity)
+{
+	std::vector<std::byte> buf;
+	cbor::codec c(buf);
+
+	/* encode */
+	c.push_back(-std::numeric_limits<float>::infinity());
+	c.push_back(-std::numeric_limits<double>::infinity());
+
+	/* verify */
+	std::array exp{
+		0xf9_b, 0xfc_b, 0x00_b,	    // fp16 -infinity
+		0xf9_b, 0xfc_b, 0x00_b	    // fp16 -infinity
+	};
+	ASSERT_EQ(buf, exp);
+
+	/* decode */
+	EXPECT_LT(c[0].get<float>(), 0);
+	EXPECT_TRUE(std::isinf(c[0].get<float>()));
+	EXPECT_LT(c[1].get<double>(), 0);
+	EXPECT_TRUE(std::isinf(c[0].get<double>()));
+
+	/* type checks */
+	for (size_t i = 0; i < size(c); ++i) {
+		EXPECT_EQ(c[i].type(), cbor::type::fp32);
+		EXPECT_THROW(c[i].get<bool>(), std::runtime_error);
+		EXPECT_THROW(c[i].get<int>(), std::runtime_error);
+	}
+}
+
+TEST(cbor, codec_int_inline)
+{
+	std::vector<std::byte> buf;
+	cbor::codec c(buf);
+
+	/* encode */
+	c.push_back(0);
+	c.push_back(23);
+	c.push_back(-1);
+	c.push_back(-24);
+
+	/* verify */
+	std::array exp{
+		0x00_b,		    // unsigned(0)
+		0x17_b,		    // unsigned(23)
+		0x20_b,		    // negative(0)
+		0x37_b,		    // negative(23)
+	};
+	ASSERT_EQ(buf, exp);
+
+	/* decode */
+	EXPECT_EQ(c[0].get<int>(), 0);
+	EXPECT_EQ(c[1].get<int>(), 23);
+	EXPECT_EQ(c[2].get<int>(), -1);
+	EXPECT_EQ(c[3].get<int>(), -24);
+
+	/* type checks */
+	for (size_t i = 0; i < size(c); ++i) {
+		EXPECT_EQ(c[i].type(), cbor::type::int32);
+		EXPECT_THROW(c[i].get<float>(), std::runtime_error);
+		EXPECT_THROW(c[i].get<bool>(), std::runtime_error);
+	}
+}
+
+TEST(cbor, codec_int_byte)
+{
+	std::vector<std::byte> buf;
+	cbor::codec c(buf);
+
+	/* encode */
+	c.push_back(24);
+	c.push_back(255);
+	c.push_back(-25);
+	c.push_back(-256);
+
+	/* verify */
+	std::array exp{
+		0x18_b, 0x18_b,	    // unsigned(24)
+		0x18_b, 0xff_b,	    // unsigned(255)
+		0x38_b, 0x18_b,	    // negative(24)
+		0x38_b, 0xff_b	    // negative(255)
+	};
+	ASSERT_EQ(buf, exp);
+
+	/* decode */
+	EXPECT_EQ(c[0].get<int>(), 24);
+	EXPECT_EQ(c[1].get<int>(), 255);
+	EXPECT_EQ(c[2].get<int>(), -25);
+	EXPECT_EQ(c[3].get<int>(), -256);
+
+	/* type checks */
+	EXPECT_THROW(c[2].get<unsigned>(), std::range_error);
+	EXPECT_THROW(c[3].get<unsigned>(), std::range_error);
+	EXPECT_THROW(c[3].get<int8_t>(), std::range_error);
+	for (size_t i = 0; i < size(c); ++i) {
+		EXPECT_EQ(c[i].type(), cbor::type::int32);
+		EXPECT_THROW(c[i].get<float>(), std::runtime_error);
+		EXPECT_THROW(c[i].get<bool>(), std::runtime_error);
+	}
+}
+
+TEST(codec, codec_int_word)
+{
+	std::vector<std::byte> buf;
+	cbor::codec c(buf);
+
+	/* encode */
+	c.push_back(256);
+	c.push_back(65535);
+	c.push_back(-257);
+	c.push_back(-65536);
+
+	/* verify */
+	std::array exp{
+		0x19_b, 0x01_b, 0x00_b,	    // unsigned(256)
+		0x19_b, 0xff_b, 0xff_b,	    // unsigned(65535)
+		0x39_b, 0x01_b, 0x00_b,	    // negative(256)
+		0x39_b, 0xff_b, 0xff_b	    // negative(65536)
+	};
+	ASSERT_EQ(buf, exp);
+
+	/* decode */
+	EXPECT_EQ(c[0].get<int>(), 256);
+	EXPECT_EQ(c[1].get<int>(), 65535);
+	EXPECT_EQ(c[2].get<int>(), -257);
+	EXPECT_EQ(c[3].get<int>(), -65536);
+
+	/* type checks */
+	EXPECT_THROW(c[2].get<unsigned>(), std::range_error);
+	EXPECT_THROW(c[3].get<unsigned>(), std::range_error);
+	EXPECT_THROW(c[3].get<int16_t>(), std::range_error);
+	for (size_t i = 0; i < size(c); ++i) {
+		EXPECT_EQ(c[i].type(), cbor::type::int32);
+		EXPECT_THROW(c[i].get<float>(), std::runtime_error);
+		EXPECT_THROW(c[i].get<bool>(), std::runtime_error);
+	}
+}
+
+TEST(codec, codec_int_dword)
+{
+	std::vector<std::byte> buf;
+	cbor::codec c(buf);
+
+	/* encode */
+	c.push_back(65536);
+	c.push_back(4294967295);
+	c.push_back(-65537);
+	c.push_back(-2147483648);
+	c.push_back(-4294967296);
+
+	/* verify */
+	std::array exp{
+		0x1a_b, 0x00_b, 0x01_b, 0x00_b, 0x00_b,	    // unsigned(65536)
+		0x1a_b, 0xff_b, 0xff_b, 0xff_b, 0xff_b,	    // unsigned(4294967295)
+		0x3a_b, 0x00_b, 0x01_b, 0x00_b, 0x00_b,	    // negative(65536)
+		0x3a_b, 0x7F_b, 0xff_b, 0xff_b, 0xff_b,	    // negative(2147483647)
+		0x3a_b, 0xff_b, 0xff_b, 0xff_b, 0xff_b	    // negative(4294967295)
+	};
+	ASSERT_EQ(buf, exp);
+
+	/* decode */
+	EXPECT_EQ(c[0].get<int>(), 65536);
+	EXPECT_EQ(c[1].get<uint32_t>(), 4294967295);
+	EXPECT_EQ(c[2].get<int>(), -65537);
+	EXPECT_EQ(c[3].get<int>(), -2147483648);
+	EXPECT_EQ(c[4].get<int64_t>(), -4294967296);
+
+	/* type checks */
+	EXPECT_EQ(c[0].type(), cbor::type::int32);
+	EXPECT_EQ(c[1].type(), cbor::type::uint32);
+	EXPECT_EQ(c[2].type(), cbor::type::int32);
+	EXPECT_EQ(c[3].type(), cbor::type::int32);
+	EXPECT_EQ(c[4].type(), cbor::type::int64);
+	EXPECT_THROW(c[2].get<unsigned>(), std::range_error);
+	EXPECT_THROW(c[3].get<unsigned>(), std::range_error);
+	EXPECT_THROW(c[4].get<unsigned>(), std::range_error);
+	EXPECT_THROW(c[4].get<int32_t>(), std::range_error);
+	for (size_t i = 0; i < size(c); ++i) {
+		EXPECT_THROW(c[i].get<float>(), std::runtime_error);
+		EXPECT_THROW(c[i].get<bool>(), std::runtime_error);
+	}
+}
+
+TEST(codec, codec_int_qword)
+{
+	std::vector<std::byte> buf;
+	cbor::codec c(buf);
+
+	/* encode */
+	c.push_back(4294967296);
+	c.push_back(18446744073709551615ull);
+	c.push_back(-4294967297);
+	c.push_back(-9223372036854775808ull);
+
+	/* verify */
+	std::array exp{
+		0x1b_b, 0x00_b, 0x00_b, 0x00_b, 0x01_b, 0x00_b, 0x00_b, 0x00_b, 0x00_b,	    // unsigned(4294967296)
+		0x1b_b, 0xff_b, 0xff_b, 0xff_b, 0xff_b, 0xff_b, 0xff_b, 0xff_b, 0xff_b,	    // unsigned(18446744073709551615)
+		0x3b_b, 0x00_b, 0x00_b, 0x00_b, 0x01_b, 0x00_b, 0x00_b, 0x00_b, 0x00_b,	    // negative(4294967296)
+		0x3b_b, 0xff_b, 0xff_b, 0xff_b, 0xff_b, 0xff_b, 0xff_b, 0xff_b, 0xff_b	    // negative(9223372036854775807)
+	};
+	ASSERT_EQ(buf, exp);
+
+	/* decode */
+	EXPECT_EQ(c[0].get<int64_t>(), 4294967296);
+	EXPECT_EQ(c[1].get<uint64_t>(), 18446744073709551615ull);
+	EXPECT_EQ(c[2].get<int64_t>(), -4294967297);
+	EXPECT_EQ(c[3].get<int64_t>(), -9223372036854775808ull);
+
+	/* type checks */
+	EXPECT_EQ(c[0].type(), cbor::type::int32);
+	EXPECT_EQ(c[1].type(), cbor::type::uint32);
+	EXPECT_EQ(c[2].type(), cbor::type::int32);
+	EXPECT_EQ(c[3].type(), cbor::type::int64);
+	EXPECT_THROW(c[2].get<unsigned>(), std::range_error);
+	EXPECT_THROW(c[3].get<unsigned>(), std::range_error);
+	EXPECT_THROW(c[3].get<int32_t>(), std::range_error);
+	for (size_t i = 0; i < size(c); ++i) {
+		EXPECT_THROW(c[i].get<float>(), std::runtime_error);
+		EXPECT_THROW(c[i].get<bool>(), std::runtime_error);
+	}
+}
